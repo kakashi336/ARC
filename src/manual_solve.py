578d01dce66cc549bc02a1d77bd31cda07df0885
kakashi336 <arjunp111009@gmail.com>
2021-11-29 17:53:22 +0000

    Commit with descriptions about functions and updated readMe file

--- src/manual_solve.py
+++ src/manual_solve.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python
 """ 
+Group Submission
+----------------
+
 Arjun prakash 
 21239525
 MSc. AI
+GitHub: https://github.com/kakashi336/ARC
 
 AND
 
 Mayank Dwivedi
 21230080
 MSc. AI
-
 GitHub: https://github.com/kakashi336/ARC
+
+
 """
+
+
+
 
 import os, sys
 import json
@@ -23,29 +31,73 @@ import re
 ### result. Name them according to the task ID as in the three
 ### examples below. Delete the three examples. The tasks you choose
 ### must be in the data/training directory, not data/evaluation.
+
+
+#---------------------Start of functions-----------------------#
+"""
+c3f564a4.json
+This function takes a grid (Which contains a pattern and blank bits) and returns the same sized grid after 
+filling the blank bits with pattern. 
+
+1. Iterate over the whole input grid and check for blanks (black value)
+2. If black color is encountered, predict the pattern by checking the right diagonal color.
+3. Assign the black pixel with the predicted value.
+4. return the final grid.
+
+Summary of features used
+For all the tasks, I have used only Numpy, and no other libraries or any pip installs.
+Completed the task using numpy array indexing. 
+
+"""
+
 def solve_c3f564a4(x):
-    data=x
+    # Function to predict bit by checking its neighbour color bits
     def predict_bit(indexof_bit):
-        if data[indexof_bit[0]-1][indexof_bit[1]+1] >0:
-            return data[indexof_bit[0]-1][indexof_bit[1]+1]
+        #Check diagonal bits and return the same color code. 
+        if x[indexof_bit[0]-1][indexof_bit[1]+1] >0:
+            return x[indexof_bit[0]-1][indexof_bit[1]+1]
         
-    for i in range(data.shape[0]):
-        for j in range(data.shape[1]):
-            if data[i][j]==0:
-                data[i][j]=predict_bit([i,j])
+    
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            # Check for blanks and predict bit color.
+            if x[i][j]==0:
+                x[i][j]=predict_bit([i,j])
+    #Return final grid
     return x
 
+# Tested the function with test-data and passed all the testcases
+
+"""
+9f236235.json
+This function takes in (m,m) sized grid and outputs (n,n) sized grid. The input grid consists of mini colored squared grids seperated
+with partition lines in both axis. The task is to identify the color of this mini grids and output a grid of size(Number of partitions+1).
+The output is to be inversed (Left mirrored image) and returned.
+
+1. Find the number of partitions and the color of partition line.
+2. Get the size of output martix (dimension= number of partition lines+1,number of partition lines+1) 
+3. Get the size of mini grids size (number of color grids between each partitions)
+4. Iterate over the grid and visit one pixel from every mini grid and assign the color to the output 
+   after inversing (mirroring the image).
+5. Return the output grid
+
+Summary of features/library used:
+For all the tasks, I have used only Numpy, and no other libraries or any pip installs.
+
+"""
 def solve_9f236235(x):
     data=x
     #Variable to store the color of partition
     partition_color=0
+    # Count the number of partition lines
     partition_count=0
     
     #traverse through the matrix
     for i in data:
         #Partition will have same color accross the row and the length of set will be one. 
         if len(set(i))==1:
-            #set partition Color.
+            
+            # Get the color of partition.
             partition_color=set(i)
             # Count the number of partition lines.
             partition_count+=1    
@@ -54,32 +106,56 @@ def solve_9f236235(x):
     #and dividing it by the size of output matrix shape.
     color_length=len(data[0]-partition_count)//(partition_count+1)
     
-    #jump variable to shift into the next color grid
+    #jump variable will be used to shift the current index to the next grid color 
     jump=len(data[0]-partition_count)//color_length
     
-    # Create output of respective size
+    # Create output of respective size and zero the data
     output=np.zeros((partition_count+1,partition_count+1),dtype=int)
 
-    # Assign the output grid color 
+    # Assign the output grid color. Traverse through the grid with step=color_length+1 (+1 for partition)
     for i in range(0,data.shape[0],color_length+1):
+        
         for j in range(0,data.shape[1],color_length+1):
+            #Formula for applying color for grid: x= i/jump+1,y=(jump-1)-(j//jump+1). This automatically takes the color
+            #from pattern and assignms to the output after mirroring.
             
             #take one pixel from each color grid and assign it to the output after inversing 0 axis 
             output[i//(jump+1)][(jump-1)-(j//(jump+1))]=data[i][j]         
     
     return output
+# Tested the function with test-data and passed all the testcases
 
+
+"""
+
+6ecd11f4.json
+The input grid (size: (m,m)) consists of a small color grid of size (n,n) and pattern of single color 'c'. The task is to output 
+a grid of size (n,n) after merging the colors with respective pattern (replace color with 0 if the pattern consists of black (color code=0)
+color pixel. 
+
+1. Get the row size,col size, all color values, their counts and index from the input grid
+2. get the color of the pattern and find the starting index of both the pattern and colored grid matrix.
+3. Calculate the size of pattern grids (by going to the index of pattern and iterating till the black color
+    is found. Store the finaliteration value.)
+4. visit each pattern pixel one time and see if the color is black. If it is black, replace the respective output
+    color to black.
+5. return the final grid
+
+Summary of features used
+For all the tasks, I have used only Numpy and no other libraries or any pip installs.
+Used numpy.unique() to get unique data,count and index from the input grid along with max() function. 
+
+"""
 def solve_6ecd11f4(x):
     data=x
-    
-    col_size,row_size=data.shape[0],data.shape[1]  #Shape of whole array
+
+    col_size,row_size=data.shape[0],data.shape[1]  #get Shape of whole input array
     # Get the colors,index and counts of unique colors
     colors,indx,counts=np.unique(data,return_counts=True,return_index=True)
     
     # Ignore the color with 0 index (Black color) and get the max count of color. This will give color of the pattern 
     a=np.where(counts[1:]==counts[1:].max())  
     color_code=a[0][0]+1  # color code of pattern pixel 
-    
     
     output_index=[]  #Index of matrix (output) first column 
     
@@ -95,16 +171,17 @@ def solve_6ecd11f4(x):
                 break     
      
     
-    # get The sliced output color matrix from the input 
+    # get The sliced color matrix from the input 
     out_matrix=data[output_index[0][0]:output_index[0][0]+len(output_index),
                     output_index[0][1]:output_index[0][1]+len(output_index)]
     
-    # starting indexes of the pattern 
+    # Store starting index of the pattern 
     x,y=indx[color_code]//row_size,indx[color_code]%row_size   
     length_of_boxes=0
     
     # Calcuate the grid dimension of pattern
     for i in range(data.shape[0]):
+        #Traverse from the starting index of where pattern starts and get the shape
         if data[x][y+i]==color_code:
             length_of_boxes+=1
         else:
@@ -117,6 +194,17 @@ def solve_6ecd11f4(x):
             if data[x+i*length_of_boxes][y+j*length_of_boxes]==0:
                 out_matrix[i][j]=0
     return out_matrix
+
+# Tested the function with test-data and passed all the testcases
+
+"""
+Commonalities:
+1. Used np.shape function frequently to iterate through the array and get the shape for indexing 
+    purpose
+2. Used calculated indexing to properly iterate and index variables
+
+
+#----------------------- End of Functions-------------------------------#
 
 
 def main():
