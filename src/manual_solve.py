638523b57fb29db38f128e32de005cb519ed36a3
kakashi336 <arjunp111009@gmail.com>
2021-11-29 14:40:22 +0000

    First commit. 3 tasks added (2 difficult, 1 medium).

--- src/manual_solve.py
+++ src/manual_solve.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python
+""" 
+Arjun prakash 
+21239525
+MSc. AI
+
+AND
+
+Mayank Dwivedi
+21230080
+MSc. AI
+
+GitHub: https://github.com/kakashi336/ARC
+"""
 
 import os, sys
 import json
@@ -10,14 +23,100 @@ import re
 ### result. Name them according to the task ID as in the three
 ### examples below. Delete the three examples. The tasks you choose
 ### must be in the data/training directory, not data/evaluation.
-def solve_6a1e5592(x):
+def solve_c3f564a4(x):
+    data=x
+    def predict_bit(indexof_bit):
+        if data[indexof_bit[0]-1][indexof_bit[1]+1] >0:
+            return data[indexof_bit[0]-1][indexof_bit[1]+1]
+        
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            if data[i][j]==0:
+                data[i][j]=predict_bit([i,j])
     return x
 
-def solve_b2862040(x):
-    return x
+def solve_9f236235(x):
+    data=x
+    #Variable to store the color of partition
+    partition_color=0
+    partition_count=0
+    
+    #traverse through the matrix
+    for i in data:
+        #Partition will have same color accross the row and the length of set will be one. 
+        if len(set(i))==1:
+            #set partition Color.
+            partition_color=set(i)
+            # Count the number of partition lines.
+            partition_count+=1    
+            
+    # Calculating the length of continous color grid by taking the length of first row, subtracting the count of partitions
+    #and dividing it by the size of output matrix shape.
+    color_length=len(data[0]-partition_count)//(partition_count+1)
+    
+    #jump variable to shift into the next color grid
+    jump=len(data[0]-partition_count)//color_length
+    
+    # Create output of respective size
+    output=np.zeros((partition_count+1,partition_count+1),dtype=int)
 
-def solve_05269061(x):
-    return x
+    # Assign the output grid color 
+    for i in range(0,data.shape[0],color_length+1):
+        for j in range(0,data.shape[1],color_length+1):
+            
+            #take one pixel from each color grid and assign it to the output after inversing 0 axis 
+            output[i//(jump+1)][(jump-1)-(j//(jump+1))]=data[i][j]         
+    
+    return output
+
+def solve_6ecd11f4(x):
+    data=x
+    
+    col_size,row_size=data.shape[0],data.shape[1]  #Shape of whole array
+    # Get the colors,index and counts of unique colors
+    colors,indx,counts=np.unique(data,return_counts=True,return_index=True)
+    
+    # Ignore the color with 0 index (Black color) and get the max count of color. This will give color of the pattern 
+    a=np.where(counts[1:]==counts[1:].max())  
+    color_code=a[0][0]+1  # color code of pattern pixel 
+    
+    
+    output_index=[]  #Index of matrix (output) first column 
+    
+    # Traverse through rows
+    for i in range(data.shape[0]):
+        #Traverse through columns
+        for j in range(data.shape[1]):
+            
+            # Find the output color matrix location
+            if data[i][j]!=color_code and data[i][j]!=0:
+                # Storing the index of the first column
+                output_index.append([i,j])
+                break     
+     
+    
+    # get The sliced output color matrix from the input 
+    out_matrix=data[output_index[0][0]:output_index[0][0]+len(output_index),
+                    output_index[0][1]:output_index[0][1]+len(output_index)]
+    
+    # starting indexes of the pattern 
+    x,y=indx[color_code]//row_size,indx[color_code]%row_size   
+    length_of_boxes=0
+    
+    # Calcuate the grid dimension of pattern
+    for i in range(data.shape[0]):
+        if data[x][y+i]==color_code:
+            length_of_boxes+=1
+        else:
+            break
+    
+    # Match the pattern and change the color of the output matrix to 0 depending on 
+    # the pattern
+    for i in range(out_matrix.shape[0]):
+        for j in range(out_matrix.shape[1]):
+            if data[x+i*length_of_boxes][y+j*length_of_boxes]==0:
+                out_matrix[i][j]=0
+    return out_matrix
 
 
 def main():
